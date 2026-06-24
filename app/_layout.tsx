0cc7e7b939bc18497aa22f199b7ad28c54076644
parvans <parvansajeevan@gmail.com>
2026-06-24 11:40:54 +0530

    added the nativewind and the started to implement the tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,27 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import { Slot, Stack } from "expo-router";
+import { FlatList, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "../global.css";
+const listItems = [
+  { id: 1, title: "Item 1" },
+  { id: 2, title: "Item 2" },
+  { id: 3, title: "Item 3" },
+  { id: 4, title: "Item 4" },
+  { id: 5, title: "Item 5" },
+  { id: 6, title: "Item 6" },
+  { id: 7, title: "Item 7" },
+  { id: 8, title: "Item 8" },
+  { id: 9, title: "Item 9" },
+  { id: 10, title: "Item 10" },
+  { id: 11, title: "Item 11" },
+  { id: 12, title: "Item 12" },
+  { id: 13, title: "Item 13" },
+  { id: 14, title: "Item 14" },
+  { id: 15, title: "Item 15" },
+];
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    <Slot />
+  )
 }
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello, World! Parvan S</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
