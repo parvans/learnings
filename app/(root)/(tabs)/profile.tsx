ee634fbba12ea027eccbf24b7e9505e9d696f670
parvans <parvansajeevan@gmail.com>
2026-07-04 11:56:17 +0530

    just add a sign out button in profile page - will chnage later

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { useAuth } from '@clerk/expo';
+import { router } from 'expo-router';
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function profile() {
+export default function Profile() {
+  const {signOut} = useAuth();
+
+  const handleSignOut = async()=>{
+    try {
+      await signOut();
+      router.replace("/sign-in");
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  }
   return (
-    <View>
+    <SafeAreaView>
       <Text>profile</Text>
-    </View>
+      <TouchableOpacity onPress={handleSignOut}>
+        <Text>Sign Out</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
 
