81c16c430d23b0630fad62422edadf112715b563
parvans <parvansajeevan@gmail.com>
2026-07-06 18:11:29 +0530

    added the supabase hook

---
+++ hooks/useSupabase.ts
@@ -0,0 +1,14 @@
+import { useAuth } from "@clerk/expo";
+import { createClerkSupabaseClient } from "../lib/supabase";
+import { useMemo } from "react";
+
+export function useSupabase(){
+    const { getToken } = useAuth();
+
+    const client = useMemo(
+        ()=>createClerkSupabaseClient(()=> getToken()), 
+        [getToken]
+    );
+
+    return client;
+}
