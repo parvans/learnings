7cdbf1813e20398e3a1c1d5942e5056a0024d47a
parvans <parvansajeevan@gmail.com>
2026-07-07 13:07:05 +0530

    added the seperate tab component for ios and android

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,6 +1,67 @@
-import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
+import { useUserStore } from "@/store/userStore";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
+import { Platform } from "react-native";
 
-export default function TabLayout() {
+function AndroidTabs() {
+  const isAdmin = useUserStore((state) => state.isAdmin);
+
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" color={color} size={size} />
+          ),
+        }}
+      />
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Add Property",
+            href: isAdmin ? undefined : null, // Disable the tab if the user is not an admin
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="add-circle" color={color} size={size} />
+            ),
+          }}
+        />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
+
+function IOSTabs() {
+  const isAdmin = useUserStore((state) => state.isAdmin);
+
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
@@ -13,6 +74,14 @@ export default function TabLayout() {
         <Label>Search</Label>
       </NativeTabs.Trigger>
 
+      {/* Create Property */}
+      {isAdmin && (
+        <NativeTabs.Trigger name="create">
+          <Icon sf="plus.circle.fill" />
+          <Label>Add Property</Label>
+        </NativeTabs.Trigger>
+      )}
+
       <NativeTabs.Trigger name="saved">
         <Icon sf="heart.fill" />
         <Label>Saved</Label>
@@ -22,7 +91,10 @@ export default function TabLayout() {
         <Icon sf="person.fill" />
         <Label>Profile</Label>
       </NativeTabs.Trigger>
-
     </NativeTabs>
   );
+}
+
+export default function TabsLayout() {
+  return Platform.OS === 'ios' ? <IOSTabs /> : <AndroidTabs />;
 }
