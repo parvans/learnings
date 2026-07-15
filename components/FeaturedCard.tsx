9a4935199181b3933ae739a00ed527cfc860b262
parvans <parvansajeevan@gmail.com>
2026-07-15 11:11:24 +0530

    image issue fixed

--- components/FeaturedCard.tsx
+++ components/FeaturedCard.tsx
@@ -20,7 +20,11 @@ export default function FeaturedCard({property}:{property:Property}) {
     //onPress={()=>router.push(`/(root)/property/${property.id}`)}
     >
         <Image
-        source={{uri: property.images[0]}}
+        source={
+            property.images.length > 0
+            ? {uri: property.images[0]}
+            :require("@/assets/images/kribb.png")
+        }
         className='w-full h-40'
         resizeMode='center'
         />
