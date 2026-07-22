276078e17b28b32ad417df9e23f225ad0b0494f1
parvans <parvansajeevan@gmail.com>
2026-07-22 10:58:39 +0530

    updates in search screen

--- app/(root)/(tabs)/search.tsx
+++ app/(root)/(tabs)/search.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-export default function search() {
+export default function Search() {
   return (
-    <View>
-      <Text>search</Text>
-    </View>
+   <SafeAreaView className='flex-1 bg-gray-50'>
+      <View className='px-5 pt-4 pb-3'>
+        <Text className='text-2xl font-bold text-gray-900 mb-4'>
+          Find Property
+        </Text>
+        <View>
+          <Text>fdfdf</Text>
+        </View>
+      </View>
+   </SafeAreaView>
+    
   )
 }
 
